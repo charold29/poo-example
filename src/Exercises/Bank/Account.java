52c81edc1b37db2d36ad1ab86a61b21bede56131
linux-charold <harolrop14@gmail.com>
2022-02-19 12:44:25 -0500

    atributos y metodos de clases para banco

--- src/Exercises/Bank/Account.java
+++ src/Exercises/Bank/Account.java
@@ -1,4 +1,27 @@
 package Exercises.Bank;
 
 public class Account {
+    private final int idAccount;
+    private double balance;
+
+    public Account(int idAccount, double balance) {
+        this.idAccount = idAccount;
+        this.balance = balance;
+    }
+
+    public int getIdAccount() {
+        return idAccount;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void depositMoney(double amount){
+        balance += amount;
+    }
+
+    public void withdrawMoney(double amount){
+        balance -= amount;
+    }
 }
--- src/Exercises/Bank/Client.java
+++ src/Exercises/Bank/Client.java
@@ -1,4 +1,39 @@
 package Exercises.Bank;
 
 public class Client {
+    private final String name;
+    private final String lastname;
+    private final String dni;
+    private final Account[] accounts;
+
+    public Client(String name, String lastname, String dni, Account[] accounts) {
+        this.name = name;
+        this.lastname = lastname;
+        this.dni = dni;
+        this.accounts = accounts;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public double consultMoney(int n){
+        return accounts[n].getBalance();
+    }
+
+    public void depositMoney(int n, double balance){
+        accounts[n].depositMoney(balance);
+    }
+
+    public void withdrawMoney(int n, double balance){
+        accounts[n].withdrawMoney(balance);
+    }
 }
